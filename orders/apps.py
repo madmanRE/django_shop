593c2a3f4507139d016b5f16d0f8b19812c8d9db
Roman <121165911+madmanRE@users.noreply.github.com>
2023-07-01 07:36:40 +0300

    Добавил корзину и заказы

--- orders/apps.py
+++ orders/apps.py
@@ -1,7 +1,6 @@
 from django.apps import AppConfig
 
 
-class CartConfig(AppConfig):
+class OrdersConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "cart"
-    verbose_name = "корзина"
+    name = "orders"
