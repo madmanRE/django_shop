2a96468a6388fa313ddaa52aaa70ac2faa897e1d
Roman <121165911+madmanRE@users.noreply.github.com>
2023-07-04 18:54:00 +0300

    реализовал рекомендательную систему

--- payment/views.py
+++ payment/views.py
@@ -5,7 +5,6 @@ from django.shortcuts import render, redirect, reverse, \
     get_object_or_404
 from orders.models import Order
 
-
 stripe.api_key = settings.STRIPE_SECRET_KEY
 stripe.api_version = settings.STRIPE_API_VERSION
 
@@ -39,6 +38,15 @@ def payment_process(request):
                 },
                 'quantity': item.quantity,
             })
+
+        if order.coupon:
+            stripe_coupon = stripe.Coupon.create(
+                name=order.coupon.code,
+                percent_off=order.discount,
+                duration='once')
+            session_data['discounts'] = [{
+                'coupon': stripe_coupon.id
+            }]
 
         session = stripe.checkout.Session.create(**session_data)
 
